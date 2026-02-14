971635ed5e86290a5c9d96bec225cce648ff4b66
lasha <maisuradze.llasha@gmail.com>
2026-02-14 17:49:56 +0400

    Add project files.

---
+++ MVC_IMDB_Practice/Data/MovieDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_IMDB_Practice.Models.Entities;
+
+namespace MVC_IMDB_Practice.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext()
+        {
+        }
+
+        public MovieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Entities.Movie> Movie { get; set; }
+    }
+}
